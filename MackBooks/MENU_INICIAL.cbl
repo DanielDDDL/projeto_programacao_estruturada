1f3587ec13bfc31ea8c3c9797a7b9b91e89a31d9
DANIEL DIAS DE LIMA <31687679@mackenzie.br>
2017-11-07 19:52:38 -0200

    itens de menus e cabecalhos alterados

--- MackBooks/MENU_INICIAL.cbl
+++ MackBooks/MENU_INICIAL.cbl
@@ -25,39 +25,52 @@
            88 OPC-VALIDA VALUE 1 2 3 4 9.
            
        77 BRANCO PIC X(20) VALUE SPACES.
+       77 LINHA  PIC X(32) VALUE ALL '='.                       
 
        PROCEDURE DIVISION.
-           ACCEPT DATA-SIST FROM DATE YYYYMMDD.
-           MOVE ANO-SIST TO ANO.
-           MOVE MES-SIST TO MES.
-           MOVE DIA-SIST TO DIA.
        
-           *> A MOSTRAR A DATA DO SISTEMA AQUI
+           PERFORM CABECALHO.
        
            PERFORM MENUPRINCIPAL UNTIL OPC = 9.
            STOP " ".
            STOP RUN.
        
+       CABECALHO.
+       
+           *> DIVISOES
+           DISPLAY LINHA AT 0302.
+           DISPLAY LINHA AT 1102.
+       
+           *> PEGANDO DATA DO SISTEMA
+           ACCEPT DATA-SIST FROM DATE YYYYMMDD.
+           MOVE ANO-SIST TO ANO.
+           MOVE MES-SIST TO MES.
+           MOVE DIA-SIST TO DIA.
+       
+           DISPLAY "BOOK'S CORP" AT 0503.
+           DISPLAY DATA-DIA AT 0523.
+           DISPLAY "MACKBOOKS" AT 0714.
+           DISPLAY "DEVS: DANIEL, RONIFER E HENRIQUE " AT 0902.
+       
        MENUPRINCIPAL.
-           
-           DISPLAY ERASE.
+          
            MOVE ZEROS TO OPC.
            
-   	       DISPLAY "(1)  ADICIONAR LIVRO".     
-   	       DISPLAY "(2)  REMOVER LIVRO".    
-   	       DISPLAY "(3)  ALTERAR LIVRO".
-           DISPLAY "(4)  CONSULTAR LIVRO".
-           DISPLAY "(9)  ENCERRAR".                                 
-   	       DISPLAY "ESCOLHA A OPCAO:".
+   	       DISPLAY "(1)  ADICIONAR LIVRO" AT 1307.     
+   	       DISPLAY "(2)  REMOVER LIVRO"   AT 1507.    
+   	       DISPLAY "(3)  ALTERAR LIVRO"   AT 1707.
+           DISPLAY "(4)  CONSULTAR LIVRO" AT 1907.
+           DISPLAY "(9)  ENCERRAR"        AT 2107.                      
+   	       DISPLAY "ESCOLHA A OPCAO:"     AT 2307.
            
            PERFORM WITH TEST AFTER UNTIL OPC-VALIDA
-           ACCEPT OPC WITH AUTO
+           ACCEPT OPC WITH AUTO AT 2325.
            IF OPC-VALIDA
                *> LIMPANDO MESANGEM DE ERRO
                DISPLAY BRANCO
-           ELSE 
+           ELSE
                *> MENSAGEM DE ERRO
-               DISPLAY "DIGITE 1, 2 OU 9"
+               DISPLAY "DIGITE 1, 2, 3, 4 OU 9"
            END-IF.
            
            EVALUATE OPC
